70c4deb815295558141b2d99721f838ccdc40ebc
MADHANG2304 <madhang2306@gmail.com>
2024-10-05 17:48:44 +0530

    Navbar Completed

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Navbar from './Pages/Navbar/Navbar'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/navbar' element = {<Navbar/>} />
+    </Routes>
+  </Router>
+)
+
 
 const App = () => {
   return (
     <div>
-      App
-    </div>
+      {routes}
+    </div> 
   )
 }
 
