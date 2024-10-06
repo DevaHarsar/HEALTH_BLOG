92ab1fa470fad600d42d3bc2095d9788cefb1b6d
Deva Harsar M M <72179089+DevaHarsar@users.noreply.github.com>
2024-10-06 18:26:37 +0530

    footer responsive for only mobile and pc

--- blog/src/Pages/Footer/Footer.jsx
+++ blog/src/Pages/Footer/Footer.jsx
@@ -3,9 +3,9 @@
 const Footer = () => {
   return (
 
-    <div className='flex flex-col  bg-black text-white py-5'>
+    <div className='w-screen flex flex-col  bg-black text-white py-5 max-sm:h-screen max-sm:w-screen'>
 
-      <div className='bg-black flex flex-row  justify-evenly font-playfair text-lg '>
+      <div className='bg-black flex flex-row  justify-evenly font-playfair text-lg max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:text-sm'>
         <div className='text-white flex flex-col justify-center items-center'>
           <img className='h-32 cursor-pointer rounded-full mb-3' src='https://media.istockphoto.com/id/1035561592/vector/vector-design-element-for-the-fitness-center.jpg?s=612x612&w=0&k=20&c=k3yyyEcqeivby9iE7gZIk33PAjtDqNsdEdYiMjw7qsM='></img>
           <p className='text-2xl font-bold font-sans cursor-pointer'>
@@ -26,22 +26,22 @@ const Footer = () => {
         </div>
 
         {/* right last footer */}
-        <div className='flex flex-col text-white font-sans justify-evenly '>
-          <div className='w-[60%] '>
+        <div className='flex flex-col text-white font-sans justify-evenly max-sm:flex max-sm:flex-col max-sm:justify-around max-sm:items-center '>
+          <div className='w-[60%]   max-sm:flex max-sm:flex-col max-sm:justify-around max-sm:items-center'>
             Subscribe here and get the latest Health tips  and Healthy Life Secrets!
           </div>
-          <div className='flex flex-col justify-between gap-2'>
+          <div className='flex flex-col justify-between gap-2 max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:gap-0'>
             <div>
               <h1>Email*</h1>
               <input type="email" placeholder='Email' className='mt-1  decoration-white placeholder:Email  text-black ' />
             </div>
-            <div className='flex flex-row  justify-between mt-2'>
+            <div className='flex flex-row  justify-between mt-2 max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center'>
               <div className='flex '>
                 <input type="checkbox" name="" id="" className="mr-2" />
                 <p>Get latest updates on NewsLetter regularly</p>
               </div>
               <div>
-                <button className='rounded-md bg-white text-black hover:bg-slate-400 hover:text-white w-32'>SUBSCRIBE</button>
+                <button className='rounded-md bg-white text-black hover:bg-slate-400 hover:text-white w-32 max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center'>SUBSCRIBE</button>
               </div>
             </div>
 
