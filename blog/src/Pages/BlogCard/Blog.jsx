50d57fef30553cf21a35355649baad108f5d8cf9
KALANDHAR <kalandars2004@gmail.com>
2024-10-07 18:47:23 +0530

    donee

--- blog/src/Pages/BlogCard/Blog.jsx
+++ blog/src/Pages/BlogCard/Blog.jsx
@@ -3,7 +3,7 @@ import Card from '../../Components/Recent_Post/Athu';
 
 const Blog = () => {
   const [items, setItems] = useState([]);
-  const [visibleItems, setVisibleItems] = useState(8); 
+  const [visibleItems, setVisibleItems] = useState(8);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,39 +15,37 @@ const Blog = () => {
   }, []);
 
   const handleViewMore = () => {
-    setVisibleItems((prevVisibleItems) => prevVisibleItems + 4); 
+    setVisibleItems((prevVisibleItems) => prevVisibleItems + 4);
   };
 
   return (
-    <>
-      <div className='mt-72'>
-        {/* Page Title */}
-        <h1 className='primary_text text-[36px] px-14 max-sm:p-0 max-sm:text-[22px] max-sm:pl-9'>
-          Recent Blogs
-        </h1>
+    <div className='mt-72'>
+      {/* Page Title */}
+      <h1 className='primary_text text-[36px] px-14 max-sm:p-0 max-sm:text-[22px] max-sm:pl-9'>
+        Recent Blogs
+      </h1>
 
-        {/* Cards Section */}
-        <div className='w-full flex justify-center items-start px-14 max-sm:w-full max-sm:p-4 '>
-          <div className='mt-10 grid gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-4 xl:grid-rows-2 lg:grid-rows-2'>
-            {items.slice(0, visibleItems).map((post, index) => (
-              <Card key={index} image={post.imageURL} title={post.description} />
-            ))}
-          </div>
-        </div>
-
-        {/* Footer Section */}
-        <div className='flex justify-center mt-20 mb-10'>
-          {visibleItems < items.length && (
-            <button
-              className='px-6 py-2 bg-[#b8953d] text-white rounded-lg hover:bg-[#b88b00] transition duration-300'
-              onClick={handleViewMore}
-            >
-              View More Posts
-            </button>
-          )}
+      {/* Cards Section */}
+      <div className='w-full flex justify-center items-start px-14 max-sm:w-full max-sm:p-4 '>
+        <div className='mt-10 grid gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-4 xl:grid-rows-2 lg:grid-rows-2'>
+          {items.slice(0, visibleItems).map((post, index) => (
+            <Card key={index} id={post.id} image={post.imageURL} title={post.description} />
+          ))}
         </div>
       </div>
-    </>
+
+      {/* Footer Section */}
+      <div className='flex justify-center mt-20 mb-10'>
+        {visibleItems < items.length && (
+          <button
+            className='px-6 py-2 bg-[#b8953d] text-white rounded-lg hover:bg-[#b88b00] transition duration-300'
+            onClick={handleViewMore}
+          >
+            View More Posts
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
