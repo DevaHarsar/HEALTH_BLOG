3273c7bcdebbef8e044b0c8e09e851a2ff14b2f8
Ranjith S <ranjithselvaraj04@gmail.com>
2024-10-08 11:48:39 +0530

    Update footer

--- blog/src/Pages/Footer/Footer.jsx
+++ blog/src/Pages/Footer/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
 
     <div className='w-screen flex flex-col  bg-black text-white py-5'>
+    <div className='w-full flex flex-col  bg-black text-white py-5'>
 
       <div className='bg-black flex flex-row  justify-evenly font-playfair text-lg max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:text-sm'>
         <div className='text-white flex flex-col justify-center items-center'>
@@ -16,8 +17,20 @@ const Footer = () => {
          {/* list items */}
         <div className='flex flex-col text-white justify-center gap-3 max-sm:mt-10 '>
            <ul className='flex flex-col gap-3 cursor-pointer'>
-          <li href="#" className='hover:text-slate-400 font-serif'>Home</li>
-          <li href="#" className='hover:text-slate-400 font-serif'>Blog</li> 
+           <li
+              className='hover:text-slate-400 font-serif cursor-pointer'
+              onClick={() => document.getElementById('home').scrollIntoView({ behavior: 'smooth' })}
+            >
+              Home
+            </li>
+
+            <li
+              className='hover:text-slate-400 font-serif cursor-pointer'
+              onClick={() => document.getElementById('blog').scrollIntoView({ behavior: 'smooth' })}
+            >
+              Blog
+            </li>
+
           <li href="#" className='hover:text-slate-400 font-serif'>Instagram</li>
           <li href="#" className='hover:text-slate-400 font-serif'>Contact</li>
           </ul>
@@ -31,7 +44,7 @@ const Footer = () => {
           <div className='flex flex-col justify-between gap-2 max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:ml-10 max-sm:mt-5'>
             <div>
               <h1>Email*</h1>
-              <input type="email" placeholder='Email' className='mt-1 px-2 decoration-white placeholder:Email hover:bg-yellow-200 text-black rounded-md' />
+              <input type="email" placeholder='Email' className='mt-1 px-2 decoration-white placeholder:Email hover:bg-orange-200 text-black rounded-md' />
             </div>
             <div className='flex flex-row  justify-between mt-2 max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:mt-5'>
               <div className='flex '>
