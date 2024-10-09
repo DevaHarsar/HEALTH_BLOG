bdd806646d15e906d773c7c5ffcf43ed4a0ffdd5
Deva Harsar M M <72179089+DevaHarsar@users.noreply.github.com>
2024-10-09 18:55:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.use(cors({ 
-    origin: 'http://localhost:5173',
+   // origin: 'http://localhost:5173',
     origin:'https://health-blog-beta.vercel.app/',
     methods: ['GET', 'POST'],
     credentials: true,
